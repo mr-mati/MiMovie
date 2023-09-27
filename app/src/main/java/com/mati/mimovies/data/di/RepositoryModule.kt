08e1fa7fa1dcb17b6e4a5d8e677be0d23cd4636c
Mati <mahdi60mm60@gmail.com>
2023-09-27 22:30:49 +0330

    Frist Commit

---
+++ app/src/main/java/com/mati/mimovies/data/di/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package com.mati.mimovies.data.di
+
+import com.mati.mimovies.data.repository.MovieRepositoryImpl
+import com.mati.mimovies.features.movies.domain.repository.MovieRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun providerMovieRepository(
+
+        repo:MovieRepositoryImpl
+
+    ):MovieRepository
+
+
+}
