48560c0fbe774e3581120ff88dc57db6de58a877
Mati <mahdi60mm60@gmail.com>
2023-09-28 15:45:30 +0330

    Base Activity

--- app/src/main/java/com/mati/mimovies/BaseApplication.kt
+++ app/src/main/java/com/mati/mimovies/BaseApplication.kt
@@ -3,8 +3,6 @@ package com.mati.mimovies
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+
 class BaseApplication : Application() {
-
-
 }
--- app/src/main/java/com/mati/mimovies/MainActivity.kt
+++ app/src/main/java/com/mati/mimovies/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.mati.mimovies.features.movies.ui.MovieScreen
 import com.mati.mimovies.ui.theme.MiMoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MovieScreen()
                 }
             }
         }
@@ -45,7 +48,7 @@ fun GreetingPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Greeting("Android")
+            MovieScreen()
         }
     }
 }
