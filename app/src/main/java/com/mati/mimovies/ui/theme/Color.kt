3ba24c83f9e12b520e00cd70cf766c83eeca0ef6
Mati <mahdi60mm60@gmail.com>
2023-09-29 16:24:17 +0330

    Edit Theme

--- app/src/main/java/com/mati/mimovies/ui/theme/Color.kt
+++ app/src/main/java/com/mati/mimovies/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.mati.mimovies.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Color1 = Color(21, 19, 24)
+val Color2 = Color(65, 79, 202)
+val Color3 = Color(21, 19, 24)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundMain = Color(21, 19, 24)
+val Blue = Color(65, 79, 202)
