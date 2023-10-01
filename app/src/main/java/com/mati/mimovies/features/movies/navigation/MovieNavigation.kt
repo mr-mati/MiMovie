965cb488f27223574767449d9c765f6271f94b64
Mati <mahdi60mm60@gmail.com>
2023-10-01 21:58:46 +0330

    Add Nav Controller

---
+++ app/src/main/java/com/mati/mimovies/features/movies/navigation/MovieNavigation.kt
@@ -0,0 +1,31 @@
+package com.mati.mimovies.features.movies.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mati.mimovies.features.movies.ui.detailScreen.MovieDetailScreen
+import com.mati.mimovies.features.movies.ui.mainScreen.MovieScreen
+import com.mati.mimovies.features.movies.ui.MovieViewModel
+import com.programming_simplified.movieapp.utils.MovieNavigationItems
+
+@Composable
+fun MovieNavigation(
+    viewModel: MovieViewModel,
+) {
+
+    val navHostController = rememberNavController()
+
+    NavHost(
+        navController = navHostController,
+        startDestination = MovieNavigationItems.MovieList.route
+    ) {
+        composable(MovieNavigationItems.MovieList.route) {
+            MovieScreen(viewModel = viewModel, navHostController = navHostController)
+        }
+        composable(MovieNavigationItems.MovieDetails.route) {
+            MovieDetailScreen(viewModel)
+        }
+    }
+
+}
