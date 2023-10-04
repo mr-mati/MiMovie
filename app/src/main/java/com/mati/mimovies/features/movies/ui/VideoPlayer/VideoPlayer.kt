d78d6a29aee1c5faf6e1a8865395b063c26e9ce5
Mati <mahdi60mm60@gmail.com>
2023-10-04 21:01:25 +0330

    Add Video Player

---
+++ app/src/main/java/com/mati/mimovies/features/movies/ui/VideoPlayer/VideoPlayer.kt
@@ -0,0 +1,39 @@
+package com.mati.mimovies.features.movies.ui.VideoPlayer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+
+@Composable
+fun VideoPlayer(
+    videoUrl: String,
+) {
+
+    val videoUrl = videoUrl
+    val context = LocalContext.current
+    val player = SimpleExoPlayer.Builder(context).build()
+    val playerView = PlayerView(context)
+    val mediaItem = MediaItem.fromUri(videoUrl)
+    val playWhenReady by rememberSaveable {
+        mutableStateOf(true)
+    }
+    player.setMediaItem(mediaItem)
+    playerView.player = player
+
+
+    LaunchedEffect(player) {
+        player.prepare()
+        player.playWhenReady = playWhenReady
+    }
+    AndroidView(factory = {
+        playerView
+    })
+
+}
