6cc739d002707a48210438a594e8eb455574e1ca
Mati <mahdi60mm60@gmail.com>
2023-10-05 14:35:05 +0330

    Edit Mapper Add Genre

--- app/src/main/java/com/mati/mimovies/features/movies/domain/mapper/MovieMapper.kt
+++ app/src/main/java/com/mati/mimovies/features/movies/domain/mapper/MovieMapper.kt
@@ -12,10 +12,11 @@ class MovieMapper @Inject constructor() : Mapper<Movies?, List<Movies.Results>?>
             Movies.Results(
                 id = it.id,
                 orginal_title = it.orginal_title,
+                title = it.title,
+                genre_ids = it.genre_ids,
                 overview = it.overview,
                 poster_path = it.poster_path,
                 vote_average = it.vote_average
-
             )
         }
     }
