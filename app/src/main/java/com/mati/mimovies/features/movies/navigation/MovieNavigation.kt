6dfd5b58ebbe2325420d4df113ec2b581970965a
Mati <mahdi60mm60@gmail.com>
2023-10-07 19:23:53 +0330

    Add IntroScreen Navigate

--- app/src/main/java/com/mati/mimovies/features/movies/navigation/MovieNavigation.kt
+++ app/src/main/java/com/mati/mimovies/features/movies/navigation/MovieNavigation.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.mati.mimovies.features.movies.ui.IntroScreen.IntroScreen
+import com.mati.mimovies.features.movies.ui.MovieViewModel
 import com.mati.mimovies.features.movies.ui.detailScreen.MovieDetailScreen
 import com.mati.mimovies.features.movies.ui.mainScreen.MovieScreen
-import com.mati.mimovies.features.movies.ui.MovieViewModel
-import com.programming_simplified.movieapp.utils.MovieNavigationItems
+import com.mati.mimovies.utils.MovieNavigationItems
 
 @Composable
 fun MovieNavigation(
@@ -18,8 +19,11 @@ fun MovieNavigation(
 
     NavHost(
         navController = navHostController,
-        startDestination = MovieNavigationItems.MovieScreen.route
+        startDestination = MovieNavigationItems.IntroScreen.route
     ) {
+        composable(MovieNavigationItems.IntroScreen.route) {
+            IntroScreen(navHostController = navHostController)
+        }
         composable(MovieNavigationItems.MovieScreen.route) {
             MovieScreen(viewModel = viewModel, navHostController = navHostController)
         }
--- app/src/main/java/com/mati/mimovies/utils/MovieNavigationItems.kt
+++ app/src/main/java/com/mati/mimovies/utils/MovieNavigationItems.kt
@@ -1,8 +1,9 @@
-package com.programming_simplified.movieapp.utils
+package com.mati.mimovies.utils
 
 sealed class MovieNavigationItems(val route:String){
 
-    object MovieScreen : MovieNavigationItems("movielist")
+    object IntroScreen : MovieNavigationItems("introScreen")
+    object MovieScreen : MovieNavigationItems("movieScreen")
     object MovieDetails : MovieNavigationItems("movieDetails")
 
 }
